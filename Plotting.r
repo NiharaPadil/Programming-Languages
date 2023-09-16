12c62f31f2be2bd561d30ff3080da1ad139c62a4
Nihara Padil <niharapadilhr@gmail.com>
2023-09-16 08:41:25 +0530

    Update Plotting.r

--- Plotting.r
+++ Plotting.r
@@ -2,14 +2,11 @@
 x = c(1, 2, 3, 4, 5)
 y = c(2, 4, 6, 8, 10)
 # Scatter Plot
-plot(x, y, type = "p", col = "blue", pch = 16, xlab = "X-axis", ylab = "Y-axis", main = "Scatter
-Plot Example")
+plot(x, y, type = "p", col = "blue", pch = 16, xlab = "X-axis", ylab = "Y-axis", main = "ScatterPlot Example")
 # Line Plot
-plot(x, y, type = "l", col = "red", lwd = 2, xlab = "X-axis", ylab = "Y-axis", main = "Line Plot
-Example")
+plot(x, y, type = "l", col = "red", lwd = 2, xlab = "X-axis", ylab = "Y-axis", main = "Line PlotExample")
 # Bar Plot
-barplot(y, names.arg = x, col = "green", xlab = "X-axis", ylab = "Y-axis", main = "Bar Plot
-Example")
+barplot(y, names.arg = x, col = "green", xlab = "X-axis", ylab = "Y-axis", main = "Bar PlotExample")
 # Histogram
 hist(y, col = "purple", xlab = "X-axis", ylab = "Frequency", main = "Histogram Example")
 
@@ -18,11 +15,9 @@ labels = c("Mumbai", "Pune", "Chennai", "Bangalore")
 piepercent= round(100 * data / sum(data), 1)
 # Plot the 2D pie chart
 pie(data, labels = piepercent, main = "City pie chart", col = rainbow(length(data)))
-legend("topright", c("Mumbai", "Pune", "Chennai", "Bangalore"), cex=0.5,
-fill=rainbow(length(data)))
+legend("topright", c("Mumbai", "Pune", "Chennai", "Bangalore"), cex=0.5,fill=rainbow(length(data)))
 
 library(plotrix)
 # Plot the 3D pie chart
 pie3D(data, labels = piepercent, main = "City pie chart", col = rainbow(length(data)))
-legend("topright", c("Mumbai", "Pune", "Chennai", "Bangalore"), cex = 0.5, 
-fill=rainbow(length(data)))
+legend("topright", c("Mumbai", "Pune", "Chennai", "Bangalore"), cex = 0.5, fill=rainbow(length(data)))
